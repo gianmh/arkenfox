30010e8358ce3dbf1decb8c7d1a16e7e703a0fee
gian <gianmh@gmail.com>
2022-06-11 07:36:34 -0400

    enabled webgl

--- user-overrides.js
+++ user-overrides.js
@@ -1,2 +1,5 @@
 // 0801: Enable search from urlbar
  user_pref("keyword.enabled", true);
+/* 4520: enable WebGL (Web Graphics Library)
+ * [SETUP-WEB] If you need it then override it. RFP still randomizes canvas for naive scripts ***/
+user_pref("webgl.disabled", false);
